836ffa17e5a41dbe415f08f58396ed270faecff3
Andrii <andreyfil1337@gmail.com>
2025-03-15 23:54:56 +0200

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,31 @@
 'use strict';
 
 function convertStrToNum(text) {
-  let numberArray = [...text].filter((symbol) => symbol !== ',');
-
-  return +numberArray.reduce((res, element) => (res += element), '');
+  return +String(text).replaceAll(',', '');
 }
 
 function convertNumToStr(number) {
-  let stringNubmer = '';
-  for (let i = `${number}`.length - 1; i >= 0; i--) {
-    stringNubmer = `${number}`[i] + stringNubmer;
-    if ((i - `${number}`.length) % 3 === 0 && i !== 0) {
-      stringNubmer = ',' + stringNubmer;
-    }
-  }
-
-  return stringNubmer;
+  return new Intl.NumberFormat('en').format(number);
 }
 
 function totalPopulation() {
-  return [...document.querySelectorAll('span.population')].reduce(
-    (sum, element) => sum + convertStrToNum(element.innerText),
-    0,
+  const totalPopulationStr = [
+    ...document.querySelectorAll('span.population'),
+  ].reduce((sum, element) => sum + convertStrToNum(element.innerText), 0);
+
+  return convertNumToStr(totalPopulationStr);
+}
+
+function avaragePopulation() {
+  return convertNumToStr(
+    Math.round(
+      convertStrToNum(totalPopulation()) /
+        [...document.querySelectorAll('span.population')].length,
+    ),
   );
 }
 
-document.querySelector('span.total-population').innerText =
-  convertNumToStr(totalPopulation());
+document.querySelector('span.total-population').innerText = totalPopulation();
 
-document.querySelector('span.average-population').innerText = convertNumToStr(
-  Math.round(
-    totalPopulation() /
-      [...document.querySelectorAll('span.population')].length,
-  ),
-);
+document.querySelector('span.average-population').innerText =
+  avaragePopulation();
